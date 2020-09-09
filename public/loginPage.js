4d4535aad8a0b48f0434d123f9be27eafd562df2
Alena Ignatyeva <ignatieva.alenka@gmail.com>
2020-09-09 21:55:11 +0300

    second commit

--- public/loginPage.js
+++ public/loginPage.js
@@ -3,5 +3,5 @@
 let user = new UserForm();
 
 user.loginFormCallback = function(data) {
-    ApiConnector.login({ login, password }, responce => responce.success ? location.reload() : this.setLoginErrorMessage(message));
+    ApiConnector.login(data, responce => responce.success ? location.reload() : this.setLoginErrorMessage(message));
 }
